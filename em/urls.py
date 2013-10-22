0aece365d064e66915e95d1a9297390a6ea74445
A. Diamond <diamond@sheprador.com>
2013-10-22 09:53:36 -0400

    Use proper urls instead of hard-coding paths

--- em/urls.py
+++ em/urls.py
@@ -7,7 +7,7 @@ api_urlpatterns = patterns(
     'emserver.em_api',
 
     # All of the API urls start with /api/v1
-    url(r'^identify_router', api_router.identify, name='identify-router')
+    url(r'^identify_router', api_router.identify, name='identify_router')
 )
 
 web_urlpatterns = patterns(
@@ -17,23 +17,23 @@ web_urlpatterns = patterns(
     # Administrative pages
 
     # Router
-    url(r'^webadmin/router/$', web_router.index, name='router-index'),
-    url(r'^webadmin/router/create/$', web_router.create, name='router-create'),
+    url(r'^webadmin/router/$', web_router.index, name='router_index'),
+    url(r'^webadmin/router/create/$', web_router.create, name='router_create'),
     url(r'^webadmin/router/(?P<pk>\d+)/$', web_router.detail,
-        name='router-detail'),
+        name='router_detail'),
     url(r'^webadmin/router/(?P<pk>\d+)/edit/$', web_router.edit,
-        name='router-edit'),
+        name='router_edit'),
     url(r'^webadmin/router/(?P<pk>\d+)/delete/$', web_router.delete,
-        name='router-delete'),
+        name='router_delete'),
 
     # RouterPage
     url(r'^webadmin/routerpage/(?P<pk>\d+)/$', web_routerpage.detail,
-        name='routerpage-detail'),
+        name='routerpage_detail'),
     url(r'^webadmin/routerpage/create/$', web_routerpage.create,
-        name='routerpage-create'),
+        name='routerpage_create'),
     url(r'^webadmin/routerpage/(?P<pk>\d+)/edit/$', web_routerpage.edit,
-        name='routerpage-edit'),
+        name='routerpage_edit'),
     url(r'^webadmin/routerpage/(?P<pk>\d+)/delete/$', web_routerpage.delete,
-        name='routerpage-delete'),
+        name='routerpage_delete'),
 
 )
