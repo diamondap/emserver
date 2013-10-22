02307c333b825726d0582d3f25bd6898f05a98fd
A. Diamond <diamond@sheprador.com>
2013-10-22 12:57:31 -0400

    Working on editable page attributes.

---
+++ em/static/js/em.js
@@ -0,0 +1,17 @@
+$(function() {
+
+    function attachModals() {
+        $('span[data-modal]').click(function showModal(evnt) {
+            var url = $(evnt.delegateTarget).attr('data-modal');
+            console.log("Modal URL = " + url);
+            $('#modal').modal({
+                'remote': url,
+                'show': true,
+                'backdrop': true,
+                'keyboard': true
+            });
+        });
+    }
+
+    attachModals();
+});
