d136a7ec8368a5b9458c8dc6671a3e216c10a13b
A. Diamond <diamond@sheprador.com>
2013-10-23 13:56:14 -0400

    Added test stubs

---
+++ em/tests/integration/views/web/routerpage_test.py
@@ -0,0 +1,19 @@
+import unittest
+import em.tests as tests
+
+class RouterPageTest(unittest.TestCase):
+
+    def test_index(self):
+        pass
+
+    def test_detail(self):
+        pass
+
+    def test_create(self):
+        pass
+
+    def test_edit(self):
+        pass
+
+    def test_delete(self):
+        pass
