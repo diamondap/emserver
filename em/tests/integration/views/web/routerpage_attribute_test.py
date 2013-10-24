6b425272941eb0ce28a18afa91dfe29ecae89a06
A. Diamond <diamond@sheprador.com>
2013-10-24 12:22:26 -0400

    No tests yet for routerpage_attribute

--- em/tests/integration/views/web/routerpage_attribute_test.py
+++ em/tests/integration/views/web/routerpage_attribute_test.py
@@ -5,17 +5,5 @@ class RouterPageAttributeTest(TestCase):
 
     fixtures = tests.DB_FIXTURES
 
-    def test_index(self):
-        pass
-
-    def test_detail(self):
-        pass
-
-    def test_create(self):
-        pass
-
     def test_edit(self):
-        pass
-
-    def test_delete(self):
         pass
